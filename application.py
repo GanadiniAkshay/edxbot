d78f04bcbacadd721d7116da60dcdbca0ec3191f
Akshay <akshaykulkarni.2104@gmail.com>
2017-10-26 03:57:58 +0530

    added base route

--- application.py
+++ application.py
@@ -4,5 +4,9 @@ from flask_wizard import Wizard
 application = Flask(__name__)
 wizard = Wizard(application)
 
+@application.route("/")
+def hello():
+    return "hello world"
+
 if __name__ == '__main__':
 	application.run()
