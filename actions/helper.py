2af0f738da9e1940d6c878d32dda771d13cb9452
Akshay <akshaykulkarni.2104@gmail.com>
2017-11-12 03:36:11 +0530

    ceritficate question fix

--- actions/helper.py
+++ actions/helper.py
@@ -6,29 +6,34 @@ def purpose(session):
     response.send(session,"'Find courses for becoming an android developer'\n\n'Find courses for learning ReactJS'\n\n'Find me a course on bootsrap'\n\n")
 
 def certificates(session):
-    response.send(session,"Here is what people think about it on Quora")
-    choices = [
-        response.template_element(
-                                    title='Quora Answer',
-                                    subtitle='Does certificates from websites like Coursera, Udacity or Edx help you get a job?',
-                                    image_url="https://lh3.googleusercontent.com/i8knN1N9o-jPD1Eckv8sXMKMPx5-_u7mFJa00JkrP-CXeTr5o9H6NDAVK_E7Iyqi6Pup=w300",
-                                    action=response.actions(type="web_url",url="https://www.quora.com/Does-certificates-from-websites-like-Coursera-Udacity-or-Edx-help-you-get-a-job"),
-                                    buttons=[response.button(type="web_url",title="Open",url="https://www.quora.com/Does-certificates-from-websites-like-Coursera-Udacity-or-Edx-help-you-get-a-job")]
-                                ),
-        response.template_element(
-                                    title='Quora Answer',
-                                    subtitle='Do employers find MOOCs certificates from Coursera or edX valuable?',
-                                    image_url="https://lh3.googleusercontent.com/i8knN1N9o-jPD1Eckv8sXMKMPx5-_u7mFJa00JkrP-CXeTr5o9H6NDAVK_E7Iyqi6Pup=w300",
-                                    action=response.actions(type="web_url",url="https://www.quora.com/Do-employers-find-MOOCs-certificates-from-Coursera-or-edX-valuable"),
-                                    buttons=[response.button(type="web_url",title="Open",url="https://www.quora.com/Do-employers-find-MOOCs-certificates-from-Coursera-or-edX-valuable")]
-                                ),
-        response.template_element(
-                                    title='Quora Answer',
-                                    subtitle='Should I apply for certification while doing MOOCs?',
-                                    image_url="https://lh3.googleusercontent.com/i8knN1N9o-jPD1Eckv8sXMKMPx5-_u7mFJa00JkrP-CXeTr5o9H6NDAVK_E7Iyqi6Pup=w300",
-                                    action=response.actions(type="web_url",url="https://www.quora.com/Should-I-apply-for-certification-while-doing-MOOCs"),
-                                    buttons=[response.button(type="web_url",title="Open",url="https://www.quora.com/Should-I-apply-for-certification-while-doing-MOOCs")]
-                                )
-    ]
-    template = response.template(type="generic",elements=choices)
-    response.send(session,template)
+    message = session['message']
+    
+    if 'certificate' in message.lower() or 'certificates' in message.lower():
+        response.send(session,"Here is what people think about it on Quora")
+        choices = [
+            response.template_element(
+                                        title='Quora Answer',
+                                        subtitle='Does certificates from websites like Coursera, Udacity or Edx help you get a job?',
+                                        image_url="https://lh3.googleusercontent.com/i8knN1N9o-jPD1Eckv8sXMKMPx5-_u7mFJa00JkrP-CXeTr5o9H6NDAVK_E7Iyqi6Pup=w300",
+                                        action=response.actions(type="web_url",url="https://www.quora.com/Does-certificates-from-websites-like-Coursera-Udacity-or-Edx-help-you-get-a-job"),
+                                        buttons=[response.button(type="web_url",title="Open",url="https://www.quora.com/Does-certificates-from-websites-like-Coursera-Udacity-or-Edx-help-you-get-a-job")]
+                                    ),
+            response.template_element(
+                                        title='Quora Answer',
+                                        subtitle='Do employers find MOOCs certificates from Coursera or edX valuable?',
+                                        image_url="https://lh3.googleusercontent.com/i8knN1N9o-jPD1Eckv8sXMKMPx5-_u7mFJa00JkrP-CXeTr5o9H6NDAVK_E7Iyqi6Pup=w300",
+                                        action=response.actions(type="web_url",url="https://www.quora.com/Do-employers-find-MOOCs-certificates-from-Coursera-or-edX-valuable"),
+                                        buttons=[response.button(type="web_url",title="Open",url="https://www.quora.com/Do-employers-find-MOOCs-certificates-from-Coursera-or-edX-valuable")]
+                                    ),
+            response.template_element(
+                                        title='Quora Answer',
+                                        subtitle='Should I apply for certification while doing MOOCs?',
+                                        image_url="https://lh3.googleusercontent.com/i8knN1N9o-jPD1Eckv8sXMKMPx5-_u7mFJa00JkrP-CXeTr5o9H6NDAVK_E7Iyqi6Pup=w300",
+                                        action=response.actions(type="web_url",url="https://www.quora.com/Should-I-apply-for-certification-while-doing-MOOCs"),
+                                        buttons=[response.button(type="web_url",title="Open",url="https://www.quora.com/Should-I-apply-for-certification-while-doing-MOOCs")]
+                                    )
+        ]
+        template = response.template(type="generic",elements=choices)
+        response.send(session,template)
+    else:
+        response.send(session,"Can you please rephrase?")
